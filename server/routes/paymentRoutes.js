01e9ce913ee7bb80114c2a9da84b3f9ee1273b72
sakib-nedariya <sakibnedariya2002@gmail.com>
2025-10-21 10:41:19 +0530

    deleted file for changes the code

--- server/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createOrder, verifyPayment } = require('../controller/paymentController');
-
-router.post('/create-order', createOrder);
-router.post('/verify-payment', verifyPayment);
-
-module.exports = router;
